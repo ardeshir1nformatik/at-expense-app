7510c38e20179f847f0310d56956db3908dbf6e9
Ardeshir Tarkeshian <ardeshir@Ardeshirs-iMac.local>
2020-11-19 18:06:41 +0100

    commit expenses

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
+import AddExpensePage from '../components/AddExpensePage'
+import EditExpensePage from '../components/EditExpensePage'
+import NotFoundPage from '../components/NotFoundPage'
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom'
+//import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory } from 'history'
+import LoginPage from '../components/LoginPage'
+import PrivateRoute from './PrivateRoute.js'
+import PublicRoute from './PublicRoute'
+
+export const history = createBrowserHistory()
+
+const AppRouter = () => (
+    <Router history={history}>
+        <div>
+            <Switch>
+                <PublicRoute path="/" component={LoginPage} exact={true} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </Router>
+)
+export default AppRouter
---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,154 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { 
+    startAddExpense, 
+    addExpense, 
+    editExpense, 
+    startEditExpense,
+    removeExpense, 
+    startRemoveExpense,
+    setExpenses, 
+    startSetExpenses
+}   from '../../actions/expenses'
+import expenses from '../fixtures/expenses'
+import database from '../../firebase/firebase'
+
+const uid = 'thisismytestuid'
+const defaultAuthState = { auth: { uid } }
+const createMockStore = configureMockStore([thunk])
+
+beforeEach((done) => {
+    const expensesData = {}
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount, createdAt }
+    })
+
+    database.ref(`users/${uid}/expenses`).set(expensesData).then(() => done())
+})
+
+test('Shoul setup remove expense action object', () => {
+    const action = removeExpense({ id: '123abc' })
+    expect(action).toEqual({      // toEqual comes from jest to compare two objects
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    })
+})
+
+test('Should remove expense from firebase', (done) => {
+    const store = createMockStore(defaultAuthState)
+    const id = expenses[2].id
+    store.dispatch(startRemoveExpense({ id })).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        })
+        return database.ref(`users/${uid}/expenses/${id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val()).toBeFalsy()
+        done()
+    })
+})
+
+test('Should setup edit expense action object', () => {
+    const action = editExpense('543hztb', { note: 'new note value' })
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '543hztb',
+        updates: { note: 'new note value' }
+    })
+}) 
+
+test('Should edit expense from firebase', (done) => {
+    const store = createMockStore(defaultAuthState)
+    const id = expenses[0].id
+    const updates = { amount: 21045}
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        })
+        return database.ref(`users/${uid}/expenses/${id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount)
+        done()
+    })
+})
+
+test('Should setup add expense action object with provided values', () => {
+    const action = addExpense(expenses[2])
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: expenses[2]
+    })
+})
+
+test('Should add expense to database and store', (done) => {
+    const store = createMockStore(defaultAuthState)
+    const expenseData = {
+        description: 'mouse',
+        amount: 3000,
+        note: 'This is better',
+        createdAt: 1000
+    }
+    store.dispatch(startAddExpense(expenseData)).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        })
+        return database.ref(`users/${uid }/expenses/${actions[0].expense.id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseData)
+        done()
+    })
+})
+
+test('Should add expense with defaults to database and store', (done) => {
+    const store = createMockStore(defaultAuthState)
+    const expenseDefaults = {
+        description: '',
+        amount: 0,
+        note: '',
+        createdAt: 0
+    }
+    store.dispatch(startAddExpense({})).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseDefaults
+            }
+        })
+        return database.ref(`users/${uid}/expenses/${actions[0].expense.id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseDefaults)
+        done()
+    })   
+})
+
+test('Should setup set expense action object with data', () => {
+    const action = setExpenses(expenses)
+    expect(action).toEqual({
+        type:  'SET_EXPENSES',
+        expenses
+    })
+})
+
+test('Should fetch the expenses from database', (done) => {
+    const store = createMockStore(defaultAuthState)
+    store.dispatch(startSetExpenses()).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'SET_EXPENSES',
+            expenses
+        })
+        done()
+    })
+})
